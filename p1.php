6ed9c7e20dae4d5b4b0509d71c19c75e7906c8fe
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-01-22 00:32:51 -0800

    updates the errors
    with numeric_error && conversion_error

--- p1.php
+++ p1.php
@@ -3,13 +3,9 @@
 <head>
 <title>Temperature Converter</title>
 <style>
-* {
-     padding:0;
-     margin:0;
- }
 
  div.wrapper{
-     width:300px;
+     width:33%;
      margin: 0 auto;
  }
 
@@ -22,111 +18,130 @@
     color: black;
 
 }
-fieldset {
-    padding:15px 20px 15px 15px;
-    border:1px solid green;
-    margin:10px 0 0 10px;
-    width: 400px;
+fieldset{
+    background-color:#F9CBC0;
+    margin-bottom:10px;
 }
-
 legend {
     font-style:italic;
     font-size:1.2em;
     color:#444;
     padding:0px 20px 0 20px;
 }
+
+div.box{
+    margin-top:10px;
+}
+
 </style>
+</head>
 <?php
-    $error="";
+
+    $numeric_error=$conversion_error="";
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if(is_numeric($_POST['initialTemp']) && !empty($_POST['selectedTemp'])){
+        //methods of showing errors!
+        if(!is_numeric($_POST['initialTemp']) || empty($_POST['initialTemp'])){
+            $numeric_error = '<p class="error">Please input a numerical value</p>';
+        } else{
+            $initalTemp = $_POST['initialTemp'];
+        }
+
+        if(empty($_POST['selectedTemp'])){
+            $conversion_error = '<p class="error">Please select your temperature conversion</p>';
+        } else{
+            $selectedTemp = $_POST['selectedTemp'];
+        }
+        
+        //making sure the $initialTemp is set and numerical also $selectedTemp is chosen
+        if(isset($_POST['initialTemp'])&&
+        is_numeric($_POST['initialTemp'])&&
+        isset($_POST['selectedTemp'])){
+
+    //initialize the converted temp variable
+     $convertedTemp ="";
+
     
-
-    //compute temperature
-    $convertedTemp ="";
-    $initalTemp = $_POST['initialTemp'];
-    $selectedTemp = $_POST['selectedTemp'];
-
-
+    // logical function of multiunit conversions
     function CtoF(){
         $convertedTemp = ($_POST['initialTemp'] * 9 / 5) + 32;
+        $convertedTemp = number_format($convertedTemp, 2);
         return $convertedTemp;
     }
 
     function FtoC(){
         $convertedTemp = ($_POST['initialTemp'] - 32) * 5 / 9;
+        $convertedTemp = number_format($convertedTemp, 2);
         return $convertedTemp;
     }
 
     function KtoC(){
         $convertedTemp = $_POST['initialTemp'] - 273.15;
+        $convertedTemp = number_format($convertedTemp, 2);
         return $convertedTemp;
     }
 
     function CtoK(){
         $convertedTemp = $_POST['initialTemp'] + 273.15;
+        $convertedTemp = number_format($convertedTemp, 2);
         return $convertedTemp;
     }
 
     function KtoF(){
         $convertedTemp = (($_POST['initialTemp'] - 273.15) * 9 / 5) + 32;
+        $convertedTemp = number_format($convertedTemp, 2);
         return $convertedTemp;
     }
 
     function FartoKel(){
         $convertedTemp = (($_POST['initialTemp'] - 32) * 5 / 9) + 273.15;
+        $convertedTemp = number_format($convertedTemp, 2);
         return $convertedTemp;
     }
 
-
+    //swith statement for different temp conversions
     switch($selectedTemp) {
         case 'CtoF':
             $convertedTemp = CtoF();
-            $outcomeTemp= "<p>You inputted $initalTemp °C is the same as $convertedTemp °F </p>";
+            $outcomeTemp= "<p>You inputted {$initalTemp}° Celcius is the same as {$convertedTemp}° Fahrenheit </p>";
             break;
         
         case 'FtoC':
             $convertedTemp = FtoC();
-            $outcomeTemp= "<p>You inputted $initalTemp °F is the same as $convertedTemp °C </p>";
+            $outcomeTemp= "<p>You inputted {$initalTemp}° Farenheit is the same as {$convertedTemp}° Celcius </p>";
             break;
 
         case 'KtoC':
             $convertedTemp = KtoC();
-            $outcomeTemp= "<p>You inputted $initalTemp °K is the same as $convertedTemp °C </p>";
+            $outcomeTemp= "<p>You inputted {$initalTemp}° Kelvin is the same as {$convertedTemp}° Celcius </p>";
             break;
         
         case 'CtoK':
             $convertedTemp = CtoK();
-            $outcomeTemp= "<p>You inputted $initalTemp °C is the same as $convertedTemp °K </p>";
+            $outcomeTemp= "<p>You inputted {$initalTemp}° Celcius is the same as {$convertedTemp}° Kelvin </p>";
             break;
         
         case 'KtoF':
             $convertedTemp = KtoF();
-            $outcomeTemp= "<p>You inputted $initalTemp °K is the same as $convertedTemp °F </p>";
+            $outcomeTemp= "<p>You inputted {$initalTemp}° Kelvin is the same as {$convertedTemp}° Fahrenheit </p>";
             break;
 
         case 'FartoKel':
             $convertedTemp = FartoKel();
-            $outcomeTemp= "<p>You inputted $initalTemp °F is the same as $convertedTemp °K </p>";
+            $outcomeTemp= "<p>You inputted {$initalTemp}° Fahrenheit is the same as {$convertedTemp}° Kelvin </p>";
             break;
 
         }
     }
-
-    else{
-        $error="Please input a numeric value for your temperature!";
-        
-    }
 }
-
-
-    //shows data
-         //shows form
-        //form goes inside here!
-
     ?>
+
+
+<!-- HTML part of the form -->
+<body>
 <div class="wrapper">
 <form action="" method="post">
+
+<h1>Multiunit Temperature Converter</h1>
         <p>Input your temperature number:  <input type="text" name="initialTemp" /></p>
 
         <fieldset>
@@ -145,10 +160,12 @@ legend {
 
         <input type="submit" />
         </form>
-
-        <div>
+    
+        <div class="box">
         <?=$outcomeTemp;?>
-        <span style="color:red"><?=$error;?></span>
+        <span style="color:red"><?=$conversion_error;?></span>
+        <span style="color:red"><?=$numeric_error;?></span>
         </div>
-
+        <a href="https://github.com/barbara-pronsato/ITC250-P1"><p>Click here for the project documentation!</p></a>
         </div>
+        </body>
